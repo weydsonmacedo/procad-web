30d7313d66bbbb8dec851c2dc9ae9e8d95ef3234
oscaresende <oscaresende>
2022-09-20 20:12:02 -0300

    ajustes

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express')
 
-const { resolve } = require('path')
+//const { resolve } = require('path')
 
 const app = express()
 
 app.use('/',
-    express.static(
-        resolve(
-            __dirname,
-            './build'
-        )
-    )
+    express.static('./build')    
 )
 
 app.listen(process.env.PORT || 3000, (err) => {
