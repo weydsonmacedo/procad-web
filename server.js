cbf9efd174e71a717ca0d7dac6951eab312f634b
oscaresende <oscaresende>
2022-09-20 23:22:53 -0300

    ajustes

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 const express = require('express')
 
-//const { path } = require('path')
-
 const app = express()
 
-// List of all the files that should be served as-is
-let protecteds = ['transformed.js', 'main.css', 'favicon.ico']
-
-app.get("*", (req, res) => {
-
-    let path = req.params['0'].substring(1)
-
-    if (protecteds.includes(path)) {
-        // Return the actual file
-        res.sendFile(`${__dirname}/build/${path}`);
-    } else {
-        // Otherwise, redirect to /build/index.html
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+    app.get('*', (req, res) => {
         res.sendFile(`${__dirname}/build/index.html`);
-    }
-});
+    });
+}
 
 app.listen(process.env.PORT || 3000, (err) => {
     if (err) { return console.log(err) }
